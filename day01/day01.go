64d501cf557699ea17c71a0d2266b0f499cb9769
Peter Welby <pwelby.mauz@gmail.com>
2020-02-27 21:01:24 -0500

    Day 1, part 2

--- day01/day01.go
+++ day01/day01.go
@@ -26,3 +26,36 @@ func Solve1(input string) (floor int) {
 	}
 	return
 }
+
+/*
+
+Solve2 solves the following AOC problem:
+
+Now, given the same instructions, find the position of the first character that
+causes him to enter the basement (floor -1). The first character in the
+instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the
+basement?
+
+*/
+func Solve2(input string) (step int) {
+	floor := 0
+	for i, r := range input {
+		step = i + 1
+		if r == '(' {
+			floor++
+		} else if r == ')' {
+			floor--
+		}
+		if floor < 0 {
+			return
+		}
+	}
+	return
+}
