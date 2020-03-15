0bbe8c019dfef9341b8b7fd008010cd59c862908
Peter Welby <pwelby.mauz@gmail.com>
2020-03-15 10:26:53 -0400

    Add Solve tests

--- day02/day02_test.go
+++ day02/day02_test.go
@@ -1,20 +1,32 @@
 package day02
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 type testdata struct {
 	input  string
 	expect int
 }
 
-func TestWrappingPaperArea(t *testing.T) {
-	var tests = []testdata{
-		{"2x3x4", 58},
-		{"1x1x10", 43},
-	}
+var wrappingPaperAreaTests = []testdata{
+	{"2x3x4", 58},
+	{"1x1x10", 43},
+}
 
-	for _, testCase := range tests {
-		l, w, h := parseLine(testCase.input)
+var ribbonLengthTests = []testdata{
+	{"2x3x4", 34},
+	{"1x1x10", 14},
+}
+
+func TestWrappingPaperArea(t *testing.T) {
+	for _, testCase := range wrappingPaperAreaTests {
+		l, w, h, err := parseLine(testCase.input)
+		if err != nil {
+			t.Error(err)
+		}
+
 		result := wrappingPaperArea(l, w, h)
 		if result != testCase.expect {
 			t.Errorf("For \"%s\": expected %d, got %d", testCase.input, testCase.expect, result)
@@ -23,16 +35,60 @@ func TestWrappingPaperArea(t *testing.T) {
 }
 
 func TestRibbonLength(t *testing.T) {
-	var tests = []testdata{
-		{"2x3x4", 34},
-		{"1x1x10", 14},
-	}
+	for _, testCase := range ribbonLengthTests {
+		l, w, h, err := parseLine(testCase.input)
+		if err != nil {
+			t.Error(err)
+		}
 
-	for _, testCase := range tests {
-		l, w, h := parseLine(testCase.input)
 		result := ribbonLength(l, w, h)
 		if result != testCase.expect {
 			t.Errorf("For \"%s\": expected %d, got %d", testCase.input, testCase.expect, result)
 		}
+	}
+}
+
+func streamifyTestData(testCases []testdata) *strings.Reader {
+	inputs := make([]string, len(testCases))
+
+	for i, testCase := range testCases {
+		inputs[i] = testCase.input
+	}
+
+	lines := strings.Join(inputs, "\n")
+
+	return strings.NewReader(lines)
+}
+
+func TestSolve1(t *testing.T) {
+	var expect int
+	input := streamifyTestData(wrappingPaperAreaTests)
+
+	for _, testCase := range wrappingPaperAreaTests {
+		expect += testCase.expect
+	}
+	res, err := Solve1(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != expect {
+		t.Errorf("In Solve1: Expected %d, got %d", expect, res)
+	}
+}
+
+func TestSolve2(t *testing.T) {
+	var expect int
+	input := streamifyTestData(ribbonLengthTests)
+
+	for _, testCase := range ribbonLengthTests {
+		expect += testCase.expect
+	}
+
+	res, err := Solve2(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != expect {
+		t.Errorf("In Solve2: Expected %d, got %d", expect, res)
 	}
 }
